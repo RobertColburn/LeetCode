7ddfa0dc279205eaf96c82d732599b72153ae201
Robert Colburn <60761375+RobertColburn@users.noreply.github.com>
2023-07-31 17:53:21 -0500

    Update addIntegers.cpp

--- cpp/2235. Add Two Integers/addIntegers.cpp
+++ cpp/2235. Add Two Integers/addIntegers.cpp
@@ -1,7 +1,12 @@
 /*
+Prompt: 
+"Given two integers num1 and num2, return the sum of the two integers."
+
+Constraints:
+-100 <= num1, num2 <= 100
+
 Robert Colburn
 7/31/2023
-Prompt: Given two integers num1 and num2, return the sum of the two integers.
 */
 
 class Solution {
