7217f077d5a5e3ced2cd7f717d00ac40ec2e6e42
Robert Colburn <60761375+RobertColburn@users.noreply.github.com>
2023-08-01 10:16:55 -0500

    Create addIntegers.cs

---
+++ cs/2235. Add Two Integers/addIntegers.cs
@@ -0,0 +1,13 @@
+/*
+Prompt: 
+"Given two integers num1 and num2, return the sum of the two integers."
+
+Robert Colburn
+8/1/2023
+*/
+
+public class Solution {
+    public int Sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
