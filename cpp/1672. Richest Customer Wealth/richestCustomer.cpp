46622bf901cc60b0868e9e1f604c46fece755e03
Robert Colburn <60761375+RobertColburn@users.noreply.github.com>
2023-08-01 13:24:13 -0500

    Create richestCustomer.cpp

---
+++ cpp/1672. Richest Customer Wealth/richestCustomer.cpp
@@ -0,0 +1,33 @@
+/*
+Prompt:
+"You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth."
+
+Constraints:
+m == accounts.length
+n == accounts[i].length
+1 <= m, n <= 50
+1 <= accounts[i][j] <= 100
+
+Robert Colburn
+8/1/2023
+*/
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxWealth = accounts[0][0];
+        int wealth;
+        for(int counter = 0; counter < accounts.size(); counter++)
+        {
+            wealth = 0;
+            for(int counter2 = 0; counter2 < accounts[counter].size(); counter2++)
+            {
+                wealth += accounts[counter][counter2];
+            }
+            if(wealth > maxWealth)
+            maxWealth = wealth;
+        }
+        return maxWealth;
+    }
+};
