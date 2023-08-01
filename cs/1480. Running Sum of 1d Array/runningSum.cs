385301e381d3ae169f791e9ae2c1a6adf8d1182e
Robert Colburn <60761375+RobertColburn@users.noreply.github.com>
2023-08-01 10:49:02 -0500

    Update runningSum.cs

--- cs/1480. Running Sum of 1d Array/runningSum.cs
+++ cs/1480. Running Sum of 1d Array/runningSum.cs
@@ -1,5 +1,13 @@
 /*
+Prompt:
+"Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]). Return the running sum of nums."
 
+Constraints:
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6
+
+Robert Colburn
+8/1/2023
 */
 
 public class Solution {
