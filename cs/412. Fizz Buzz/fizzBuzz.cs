b6eeac2305f522950b9dd6e8d720a79779311de4
Robert Colburn <60761375+RobertColburn@users.noreply.github.com>
2023-08-02 09:43:54 -0500

    Update fizzBuzz.cs

--- cs/412. Fizz Buzz/fizzBuzz.cs
+++ cs/412. Fizz Buzz/fizzBuzz.cs
@@ -1,3 +1,18 @@
+/*
+Prompt:
+"Given an integer n, return a string array answer (1-indexed) where:
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true."
+
+Constraints:
+1 <= n <= 104
+
+Robert Colburn
+8/2/2023
+*/
+
 public class Solution {
     public IList<string> FizzBuzz(int n) {
         string[] answer = new string[n];
