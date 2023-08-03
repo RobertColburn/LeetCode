02159c9f887c68a7433492f961e1aee3d05c9c20
Robert Colburn <60761375+RobertColburn@users.noreply.github.com>
2023-08-03 12:48:34 -0500

    Update reduceToZero.cs

--- cs/1342. Number of Steps to Reduce a Number to Zero/reduceToZero.cs
+++ cs/1342. Number of Steps to Reduce a Number to Zero/reduceToZero.cs
@@ -1,4 +1,14 @@
+/*
+Prompt:
+"Given an integer num, return the number of steps to reduce it to zero.
+In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it."
 
+Constraints:
+0 <= num <= 106
+
+Robert Colburn
+8/3/2023
+*/
 
 public class Solution {
     public int NumberOfSteps(int num) {
