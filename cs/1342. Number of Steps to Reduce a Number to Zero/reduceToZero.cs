0c00160475cd1ea064a989a9c0ba1fe4eeaa3a2f
Robert Colburn <60761375+RobertColburn@users.noreply.github.com>
2023-08-04 12:40:17 -0500

    Update reduceToZero.cs

--- cs/1342. Number of Steps to Reduce a Number to Zero/reduceToZero.cs
+++ cs/1342. Number of Steps to Reduce a Number to Zero/reduceToZero.cs
@@ -16,7 +16,7 @@ public class Solution {
         while(num != 0)
         {
             if(num % 2 == 0)
-            num = num / 2;
+            num /= 2;
              else
             num -= 1;
             steps++;
